aa256271d1b2fecc6fdfc4cf6f7a00e32ab08347
dbo99 <oconnor.david.b@gmail.com>
2019-04-08 12:56:04 -0700

    Add files via upload

---
+++ fun_defs.r
@@ -0,0 +1,65 @@
+# Read an individual block
+readBlock = function(text){
+  basin = str_replace(string = text[1], pattern = "^:", replacement = "")
+  block = text[-1]
+  code = str_match(block[1], "[A-Z0-9]{5}")[1]
+  block = str_replace(block, "^(:?[^ ]+|:)", "")
+  block = str_replace(block, "%", "(%)")
+  block = str_replace_all(block, "[;():]", "|")
+  block = trimws(block)
+  block = str_split(block,"\\|")
+  block = as.data.frame(do.call(rbind, block))
+  colnames(block) = c("Value","Calc", "Units", "Location")
+  block$Code = code
+  block$Basin = basin
+  return(block)
+}
+
+# Find blocks starting index
+findBlocks = function(text){
+  index = which(str_detect(text,"^:?[A-Z0-9]{5}"))
+  index = index[index > 10]
+  index = index - 1
+  index = c(index, 1 + which(str_detect(text,"\\.END")))
+  return(index)
+}
+
+# return a data frame with all blocks
+readAllBlocks = function(index, text){
+  blocks = lapply(1:(length(index)-1), function(x){
+    blockText = text[index[x]:(index[x+1]-2)]
+    readBlock(blockText)
+  })
+  blocks = do.call(rbind, blocks)
+  return(blocks)
+}
+
+
+#####################################
+### time classifications ##
+#####################################
+
+water_year <- function(date) {
+  ifelse(month(date) < 10, year(date), year(date)+1)}
+
+water_month <-function(date) {
+  ifelse(month(date) < 10, month(date)+3, month(date)-9)}
+
+
+df <- df %>% mutate(yday = yday(date), year = year(date))
+
+df_yr <- df %>% group_by(year) %>%  summarize(cumdoy= max(yday, na.rm=T))
+
+
+add_timeclasses <- function(df) {
+  df <- df %>% mutate(year = year(date), yday = yday(date))
+  df_yr <- df %>% group_by(year) %>%  summarize(cumdoy= max(yday, na.rm=T))
+  df <- merge(df, df_yr, by.x = "year", by.y = "year", all.x=TRUE)
+  df <- df %>% mutate(dowy =   ifelse(cumdoy > 365, 
+                               ifelse(yday>274, yday-274, yday+92), 
+                               ifelse(yday>273, yday-273, yday+92)),
+                               wy = water_year(date), wm =water_month(date)) %>% select(-cumdoy)
+
+}
+
+
--- wy19nohrsc_dfbuild.r
+++ wy19nohrsc_dfbuild.r
@@ -2,39 +2,39 @@ rm(list = ls())
 library(tidyverse)
 library(rvest)
 library(lubridate)
-library(gganimate)
-library(gifski)
-library(transformr)
-library(tweenr)
+#library(gganimate)
+#library(gifski)
+#library(transformr)
+#library(tweenr)
 library(sf)
 library(rgdal)
-library(ggrepel)
+#library(ggrepel)
 #library(cowplot)
 
-setwd("~/Documents/Rspat")
+setwd("~/R/proj/nohrsc")
+#setwd("~/Documents/Rspat")
 source("fun_defs.r")
 source("df_init.r")
 params <- c ("swe",
              "sca", 
-           "se", 
+             "se", 
              "sm", 
              "sb", 
              "sd", 
              "su")
-paramnams <- c("Snow Water Equivalent [in]",
-                "Areal Extent of Basin Snow Cover [%]",
-                "Average Snowpack Temperature [F]",
-                "Snow Melt [in]", 
-                "Blowing Snow Sublimation [in]" , 
-                "Snow Depth [in]", 
-                "Snow Surface Sublimation [in]" )
+paramnams <- c("Snow Water Equivalent [in]" ,
+               "Areal Extent of Basin Snow Cover [%]",
+               "Average Snowpack Temperature [F]",
+               "Snow Melt [in]", 
+               "Blowing Snow Sublimation [in]" , 
+               "Snow Depth [in]", 
+               "Snow Surface Sublimation [in]" )
 
 
-start_date <- ymd("2018-10-01")
-end_date <- ymd("2019-04-07")
+start_date <- ymd("2016-10-01")
+end_date <- ymd("2017-09-30")
 
 dates <- seq(start_date, end_date, by = "days") %>% rev()
-
 
 
 for (i in 1:length(dates)) { 
@@ -43,8 +43,8 @@ for (i in 1:length(dates)) {
   monthnum <- as.character(month(dates[i]))
   dayofmon <- as.character(day(dates[i]))
   
-        for (j in 1:length(params)) {
-
+  for (j in 1:length(params)) {
+    
     url1 <- "https://www.nohrsc.noaa.gov/shef_archive/index.html?rfc=cnrfc&product="
     url2 <- params[j]
     url3 <- "&year="
@@ -59,23 +59,25 @@ for (i in 1:length(dates)) {
     
     #swe = "https://www.nohrsc.noaa.gov/shef_archive/index.html?rfc=cnrfc&product=swe&year=2019&month=3&day=27&hour=12"
     urlfin <- paste0(url1, url2, url3, url4, url5, url6, 
-                                  url7, url8, url9)
+                     url7, url8, url9)
     # scrape
     #if (url8 == 1 | url8 == 5 | url8 == 10 | url8 == 15 | url8 == 20 | url8 == 25 ) { Sys.sleep(0.5) }
-    if((url8 %% 2) == 0) {Sys.sleep(1) }
-    if((url8 %% 9) == 0) {Sys.sleep(3) }
-   # if (url8 == 3) { Sys.sleep(3.5) }
-
+    num = as.integer(url8)
+    if(num %% 2 == 0) {Sys.sleep(0.5) } #if even (no remainder),give  break
+    if(num %% 9 == 0) {Sys.sleep(1.5) }# if 9,18,27th day of month, give more break (all to not overload server)
+    # if (url8 == 3) { Sys.sleep(3.5) }
+    
     scrapedtext = html_text(html_node(read_html(urlfin),".notes"))
     scrapedtext = unlist(str_split(scrapedtext,"\n"))
     block_index = findBlocks(scrapedtext)
     dftemp_i = readAllBlocks(block_index, scrapedtext) %>% mutate( param = params[j], date = dates[i])
     
-
-    df <- rbind(df,dftemp_i) %>% unique()
-
- 
-        }  
+    
+    df <- rbind(df,dftemp_i) %>% unique() %>% na.omit()
+    
+  }  
 }
 
-write_csv(df, "wy2019_allp_thruApr7.csv")
+df1 <- add_timeclasses(df)
+
+write_csv(df, "wy2017_allp_thruApr7.csv")
